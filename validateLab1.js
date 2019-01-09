d08721f49c8cd0a04310b46102340202359666dc
Daniel Poulson <danielp@deltalabs.com.au>
2019-01-10 08:01:14 +1100

    Update

--- validateLab1.js
+++ validateLab1.js
@@ -1,4 +1,4 @@
-var pipeline = [
+var pipeline2 = [
   {
     $match: {
       "imdb.rating": { $gte: 7 },
@@ -6,5 +6,17 @@ var pipeline = [
       $or: [{ rated: "PG" }, { rated: "G" }],
       $and: [{ languages: "English" }, { languages: "Japanese" }]
     }
+  },
+  {
+    $project: { title: 1, rated: 1, _id: 0 }
+  }
+];
+
+var pipeline = [
+  {
+    $project: { title: { $size: { $split: ["$title", " "] } } }
+  },
+  {
+    $match: { title: 1 }
   }
 ];
